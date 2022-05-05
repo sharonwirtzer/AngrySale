7b386c137ca5d1cbc0b8761e4188f75781d53aaa
sharonwirtzer <sharonwirtzer@gmail.com>
2022-05-05 23:06:15 +0300

    new img and opacity

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-
-const imgs = [
-    {
-        id: 5,
-        title: "Picnic Tote Bag",
-        description: "Insulated cooler - 4 colors",
-        price: "$18.99",
-        pictureUrl: "img/p1.png",
-    },
-    {
-        id: 6,
-        title: "Beach Hat",
-        description: "Your nose will never be sunburnt again - 3 colors",
-        price: "$19.99",
-        pictureUrl: "img/p2.png"
-    },
-    {
-        id: 7,
-        title: "Fairy Lights Hat",
-        description: "Be the life of the party  - 2 colors",
-        price: "$18.99",
-        pictureUrl: "img/p3.png"
-    }
-];
-
 const cards = [
     {
         id: 1,
@@ -59,13 +34,37 @@ const cards = [
     }
 ];
 
+const imgs = [
+    {
+        id: 5,
+        title: "Picnic Tote Bag",
+        description: "Insulated cooler - 4 colors",
+        price: "$18.99",
+        pictureUrl: "img/p1.png",
+    },
+    {
+        id: 6,
+        title: "Beach Hat",
+        description: "Your nose will never be sunburnt again - 3 colors",
+        price: "$19.99",
+        pictureUrl: "img/p2.png"
+    },
+    {
+        id: 7,
+        title: "Fairy Lights Hat",
+        description: "Be the life of the party  - 2 colors",
+        price: "$18.99",
+        pictureUrl: "img/p3.png"
+    }
+];
+
 function renderCards(card) {
     return `
     <article>
-      <div>
       <h6>
       <h4>${card.extra}<spam> ${card.DiscountNum} </spam>${card.off}</h4>
       </h6>
+      <div>
       <img src="${card.pictureUrl}">
       </div>
       <div>
@@ -77,26 +76,22 @@ function renderCards(card) {
 
 function renderImgs(img) {
     return `
-  <section>
-    <div>
+    <section>
     <img src="${img.pictureUrl}">
-    </div>
-    <div class="description_container" >
+    <div class="description_container">
     <h1> ${img.title} </h1>
-    <h2>${img.description} </h2>
+    <h2>${img.description}</h2>
     <h3 class="img_price"> ${img.price} 
     <img src="img/arrow.png"></h3>
     </div>
   </section>
-
 `;
 }
 
-function setContainer(id,data) {
+function setContainer(id, data) {
     const container = document.querySelector(id);
     container.innerHTML = data.join(" ");
 }
 
 setContainer(".imgs_container", imgs.map(renderImgs))
-setContainer(".cards_container", cards.map(renderCards)
-)
+setContainer(".cards_container", cards.map(renderCards))
